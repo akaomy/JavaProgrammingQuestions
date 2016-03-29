c98cfb8dc77bbb0ddba2dc8c34f1d30e23e18d13
Alex Barabash <a-abarabash@expedia.com>
2016-03-29 15:55:59 -0700

    FizzBuzz and PrintTriangle

---
+++ src/main/java/Day1/PrintTriangle.java
@@ -0,0 +1,59 @@
+package Day1;
+
+import org.junit.Test;
+
+/**
+ * Created by abarabash on 3/29/16.
+ */
+public class PrintTriangle {
+
+    public static void printTriRecursion(int currHeight, int height) {
+
+        if (currHeight == 1) {
+            printSpaces(currHeight);
+            printDots(height - currHeight);
+        } else {
+
+            printSpaces(currHeight);
+            printDots(height - currHeight);
+
+            printTriRecursion(currHeight - 1, height);
+        }
+    }
+
+    public static void printTriangle(int height) {
+
+        for (int i = 0; i < height; i++) {
+            printSpaces(height - i);
+            printDots(i);
+        }
+    }
+
+    public static void printSpaces(int height) {
+        for (int i = 0; i < height; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printDots(int dots) {
+        for (int i = 0; i <= dots; i++) {
+            System.out.print(". ");
+        }
+        System.out.print("\n");
+    }
+
+
+    @Test
+    public void test001() {
+        printTriangle(4);
+    }
+
+    @Test
+    public void test002() {
+        printTriRecursion(5, 5);
+    }
+
+
+}
+
+
