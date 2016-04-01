c9c4f9371d4183c75a4c013c2dcabfdbfaab5d6e
Alex Barabash <a-abarabash@expedia.com>
2016-04-01 16:20:20 -0700

    howManyChar

--- src/main/java/Day1/HowManyUniqueChar.java
+++ src/main/java/Day1/HowManyUniqueChar.java
@@ -11,28 +11,28 @@ import java.util.Map;
 public class HowManyUniqueChar {
 
     public void printResult(HashMap<Character, Integer> map){
-        for (Map.Entry<Character, Integer> each : map.entrySet()){
-            System.out.println(each);
+        for (Map.Entry<Character, Integer> each : map.entrySet()){ //iterating through entrySet of key-value pair
+            System.out.println(each); //printing each pair
         }
     }
 
-    public void howManyEachCharInSent(String string) {
+    public void howManyEachCharInSent(String input) {
 
-        HashMap<Character, Integer> result = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> result = new HashMap<Character, Integer>(); //declare result variable
 
-        string = string.toLowerCase();
-        string = string.replace(" ", "");
+        input = input.toLowerCase(); //making all characters lower case
+        input = input.replace(" ", ""); //deleting spaces
 
-        for (char each : string.toCharArray()) {
-            if (result.containsKey(each)) {
-                int value = result.get(each);
-                result.put(each, (value + 1));
+        for (char each : input.toCharArray()) { //traversing through input string
+            if (result.containsKey(each)) { //if result HashMap already contains character
+                int value = result.get(each); //getting the value stored in Map
+                result.put(each, (value + 1));//incrementing value by 1
             } else {
-                result.put(each, 1);
+                result.put(each, 1); //else if there is not such a character in result Map - add it
             }
         }
 
-        printResult(result);
+        printResult(result); //use existing method to printout result
     }
 
     @Test
