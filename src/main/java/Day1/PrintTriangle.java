b4012a1fd4acd7183e939885de2f9780b4be6b65
Alex Barabash <a-abarabash@expedia.com>
2016-04-02 11:00:46 -0700

    sort array of zeroes

--- src/main/java/Day1/PrintTriangle.java
+++ src/main/java/Day1/PrintTriangle.java
@@ -23,23 +23,24 @@ public class PrintTriangle {
 
     public static void printTriangle(int height) {
 
-        for (int i = 0; i < height; i++) {
-            printSpaces(height - i);
-            printDots(i);
+        for (int i = 0; i < height; i++) { //for each level of triangle
+            printSpaces(height - i); // we need first print spaces before "dots"
+            printDots(i); // and then print dots // first level has 1 dot, second - 2, etc.
         }
     }
 
     public static void printSpaces(int height) {
-        for (int i = 0; i < height; i++) {
+        for (int i = 0; i < height; i++) { //depends on the height value, we printing out amount of spaces
             System.out.print(" ");
         }
     }
 
     public static void printDots(int dots) {
-        for (int i = 0; i <= dots; i++) {
-            System.out.print(". ");
+        for (int i = 0; i <= dots; i++) { ///depends on the dots value,
+            System.out.print(". "); //  we printing out amount of dots
         }
-        System.out.print("\n");
+
+        System.out.print("\n"); //after each level of triangle we need to start new line
     }
 
 
---
+++ src/main/java/Day1/RussianRulette.java
@@ -0,0 +1,29 @@
+package Day1;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * Created by abarabash on 4/1/16.
+ */
+public class RussianRulette {
+
+        @Test
+        public void shoot600000times() {
+
+            int count = 0;
+            int[] a = new int[]{0, 0, 0, 0, 0, 1};
+
+            for (int i = 0; i < 600000; i++){
+                Random random = new Random();
+                int k = random.nextInt(6);
+                if (a[k] == 1){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+
+
+}
